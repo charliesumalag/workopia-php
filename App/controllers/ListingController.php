ab1333907022b1eef633b5fbcaa58ff69ffdbc53
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-01-08 15:50:12 +0800

    handling route params

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -42,15 +42,19 @@ class ListingController
      *
      * @return void
      */
-    public function show()
+    public function show($params)
     {
-        $id = $_GET['id'] ?? '';
+        $id = $params['id'] ?? '';
 
         $params = [
             'id' => $id
         ];
-        // inspect($id);
         $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
         loadView('listings/show', [
             'listing' => $listing
         ]);
--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ require basePath('routes.php'); //registering routes
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 // $uri = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
 
 
-$router->route($uri, $method); //running the route method inside the Router Class
+
+$router->route($uri); //running the route method inside the Router Class
