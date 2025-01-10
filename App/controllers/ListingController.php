0d8054b30e3cdf775f63852a0c2ff0a2a47f5d4c
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-01-11 05:16:55 +0800

    Insert Listings into Database

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -73,7 +73,7 @@ class ListingController
         $newListingData['user_id'] = 1;
 
         $newListingData = array_map('sanitize', $newListingData);
-        $requiredFields =  ['title', 'description', 'email', 'city', 'state'];
+        $requiredFields =  ['title', 'description', 'email', 'city', 'state', 'salary'];
         $errors = [];
 
         foreach ($requiredFields as $field) {
@@ -89,7 +89,27 @@ class ListingController
             ]);
         } else {
             //Submit data
-            echo 'success';
+
+
+            $fields = [];
+
+            foreach ($newListingData as $field => $value) {
+                $fields[] = $field;
+            }
+            $fields = implode(', ', $fields);
+
+            $values = [];
+            foreach ($newListingData as $field => $value) {
+                if ($value === '') {
+                    $newListingData[$field] = null;
+                }
+                $values[] = ':' . $field;
+            }
+            $values = implode(',', $values);
+            $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+            $this->db->query($query, $newListingData);
+
+            redirect('/listings');
         }
     }
 }
